438066456cd2aa069ac0f04bccb794d7dcb93f0c
TOLGA <195678116+T0LGADEMIR@users.noreply.github.com>
2025-07-17 17:44:54 +0300

    hafta 1 ödevleri yapıldı.

---
+++ src/week1/ManavKasaProgramı.java
@@ -0,0 +1,38 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ManavKasaProgramı {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        double Armut = 2.14;
+        double Elma = 3.67;
+        double Domates = 1.11;
+        double Muz = 0.95;
+        double Patlıcan = 5.00;
+
+
+        System.out.println("Armut Kaç Kilo?");
+        double armutKG = input.nextDouble();
+
+        System.out.println("Elma Kaç Kilo?");
+        double elmaKG = input.nextDouble();
+
+        System.out.println("Domates Kaç Kilo?");
+        double domatesKG = input.nextDouble();
+
+        System.out.println("Muz Kaç Kilo?");
+        double muzKG = input.nextDouble();
+
+        System.out.println("Patlıcan Kaç Kilo?");
+        double patlıcanKG = input.nextDouble();
+
+        double toplamTutar = (armutKG * Armut) + (elmaKG * Elma) + (domatesKG * Domates) + (muzKG * Muz) + (patlıcanKG * Patlıcan);
+        System.out.printf("Toplam Tutar: %.2f TL\n", toplamTutar);
+
+        input.close();
+    }
+}
--- src/week1/VucutKitleIndeksi.java
+++ src/week1/VucutKitleIndeksi.java
@@ -1,4 +1,21 @@
 package week1;
 
+import java.util.Scanner;
+
 public class VucutKitleIndeksi {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen boyunucu (metre cinsinden) giriniz: ");
+        double boy = scanner.nextDouble();
+
+        System.out.println("Lütfen kilonuzu giriniz: ");
+        double kilo = scanner.nextDouble();
+
+        double index = kilo / (boy * boy);
+        System.out.println("Vücüt Kitle İndeksiniz: " + index);
+
+        scanner.close();
+    }
 }
